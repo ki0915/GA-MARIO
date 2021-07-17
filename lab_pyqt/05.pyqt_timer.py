ae858a3dc86b3ba5ea2b9574a98b879ab7fa5a5e
minskkoasdsadgfsdkl <86870992+minskkoasdsadgfsdkl@users.noreply.github.com>
2021-07-17 14:59:13 +0900

    Create 05.pyqt_timer.py

---
+++ lab_pyqt/05.pyqt_timer.py
@@ -0,0 +1,35 @@
+# 05.pyqt_timer.py
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QTimer
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        #창의 크기 고정
+        self.setFixedSize(1024, 768)
+
+        #창 제목
+        self.setWindowTitle('GA Mario')
+
+        #타이머 생성
+        qtimer = QTimer(self)
+        #타이머에 실행할 함수 연결
+        qtimer.timeout.connect(self.timer)
+        # 1초마다 연결된 함수를 실행
+        qtimer.start(10)
+
+        #창 띄우기
+        self.show()
+
+    # 주기적으로 실행할 함수
+    def timer(self):
+        print('timer')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyApp()
+    sys.exit(app.exec())
